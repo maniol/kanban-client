7f5ef64e5f9b06ebbdf6986bd1c823f3f86ad721
magda <magdalena.aniol@gmail.com>
2018-09-01 17:27:44 +0200

    add cleaned create-react-app generated files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+/*ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();*/
+
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'))
